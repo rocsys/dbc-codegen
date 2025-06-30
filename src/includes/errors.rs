92c8cec6f7e00ef9b056fb4ea9ff7c56aaa7961a
Floris Romeijn <fromeijn@rocsys.com>
2025-06-30 14:03:01 +0200

    refactor: fix clippy waring rust 1.88

--- src/includes/errors.rs
+++ src/includes/errors.rs
@@ -20,6 +20,6 @@ pub enum CanError {
 
 impl core::fmt::Display for CanError {
     fn fmt(&self, f: &mut core::fmt::Formatter<'_>) -> core::fmt::Result {
-        write!(f, "{:?}", self)
+        write!(f, "{self:?}")
     }
 }
